efc9d01836adb6749b5acf19575302e7f4949fd0
Pedro Machado <machado.trabalhos@gmail.com>
2025-11-01 23:14:17 -0300

    good for today

---
+++ 2-chat-app/backend/src/controllers/auth.controller.js
@@ -0,0 +1,56 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    //validation
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      //encurtar pra nao escrever fullname:fullname
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      //generate jwt token
+      generateToken(newUser._id, res); //mongodb stores as _id
+      await newUser.save();
+      res
+        .status(201)
+        .json({
+          id: newUser._id,
+          email: newUser.email,
+          fullName: newUser.fullName, 
+          profilePic: newUser.profilePic,
+        });
+    } else {
+      return res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller",error.message)
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const login = (req, res) => {
+  res.send("login route");
+};
+export const logout = (req, res) => {
+  res.send("logout route");
+};
