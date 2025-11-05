d7e67185feee8bb37691bdaf5309089b9f41bf48
Pedro Machado <machado.trabalhos@gmail.com>
2025-11-05 18:23:51 -0300

    backend and most front

--- 2-chat-app/backend/src/index.js
+++ 2-chat-app/backend/src/index.js
@@ -1,18 +1,31 @@
-import express from "express"
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js"
-import { connectDB } from "./lib/db.js"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-dotenv.config()
-const app = express()
+import { connectDB } from "./lib/db.js";
 
-const PORT = process.env.PORT
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
-app.use(express.json())
+dotenv.config();
+const app = express();
 
-app.use("/api/auth", authRoutes)
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
-    console.log("Server started at http://localhost:" + PORT);
-    connectDB();
-})
+  console.log("Server started at http://localhost:" + PORT);
+  connectDB();
+});
